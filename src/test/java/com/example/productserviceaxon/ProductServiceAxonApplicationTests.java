fae952578a87c898b63cb30665edba0d4650dd84
Panupong Keawkam <63070132@kmitl.ac.th>
2022-10-11 11:06:07 +0700

    Initial commit

---
+++ src/test/java/com/example/productserviceaxon/ProductServiceAxonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productserviceaxon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceAxonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
